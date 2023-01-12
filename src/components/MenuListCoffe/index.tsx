30c677e68c56dc010d446b04a99108bc9c0002f9
*Gustavo <guga77sugu@hotmail.com>
2023-01-12 17:45:29 -0300

    menuListCoffee.module 90%

--- src/components/MenuListCoffe/index.tsx
+++ src/components/MenuListCoffe/index.tsx
@@ -1,8 +1,7 @@
 import styles from './menuListCoffee.module.css'
 import { COFFEE_IMAGES } from '../../constants/coffeeImgs'
 import dados from '../../mocks/dados.json'
-
-
+import { FileX } from 'phosphor-react';
 
 export function MenuListCoffee() {
     return (
@@ -12,15 +11,42 @@ export function MenuListCoffee() {
 
             <div className={styles.menuCards}>
                 {dados.map((coffee) => {
+                    if (!(coffee.id in COFFEE_IMAGES)) {
+                        return null;
+                    }
                     return (
 
                         <div className={styles.card}>
-                            <img src={COFFEE_IMAGES[coffee.id]} alt="" />
+                            <img src={COFFEE_IMAGES[coffee.id as keyof typeof COFFEE_IMAGES]} style={{
+                                width: "7.5rem",
+                                height: "7.5rem",
+                                marginTop: "-1.25rem"
+                            }} alt="" />
 
-                            <span>TRADICIONAL</span>
+                            <div className={styles.tags}>
+                                {coffee.tags.map((tag) => {
+                                    return (
+                                        <span
+                                            style={{
+                                                background: "var(--yellow-light",
+                                                color: "var(--yellow-dark)",
+                                                borderRadius: "50px",
+                                                fontSize: "0.625rem",
+                                                fontWeight: 700,
+                                                padding: "0.2rem 0.5rem"
+                                            }}>
+                                            {tag}
+                                        </span>
+                                    )
+
+                                })}
+                            </div>
 
                             <h3>{coffee.title}</h3>
-                        </div>)
+
+                            <p>{coffee.description}</p>
+                        </div>
+                    )
                 })}
             </div>
 
