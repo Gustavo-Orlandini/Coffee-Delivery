cac4a8659fa32017cf0fbcfa375006160583068b
*Gustavo <guga77sugu@hotmail.com>
2023-01-12 15:43:58 -0300

    Dados.json include

--- src/components/MenuListCoffe/index.tsx
+++ src/components/MenuListCoffe/index.tsx
@@ -1,12 +1,31 @@
 import styles from './menuListCoffee.module.css'
+import { COFFEE_IMAGES } from '../../constants/coffeeImgs'
+import dados from '../../mocks/dados.json'
+
+
 
 export function MenuListCoffee() {
     return (
-        <div className={styles.menu}>
+        <div className={styles.menuContainer}>
 
             <h2>Nossos cafés</h2>
 
+            <div className={styles.menuCards}>
+                {dados.map((coffee) => {
+                    return (
+
+                        <div className={styles.card}>
+                            <img src={COFFEE_IMAGES[coffee.id]} alt="" />
+
+                            <span>TRADICIONAL</span>
+
+                            <h3>{coffee.title}</h3>
+                        </div>)
+                })}
+            </div>
+
         </div>
+
 
     )
 }
---
+++ src/constants/coffeeImgs.ts
@@ -0,0 +1,32 @@
+import ArabeImg from '../assets/cups/arabe.svg'
+import ExpressoAmericanoImg from '../assets/cups/expresso-americano.svg'
+import CafeComLeiteImg from '../assets/cups/cafe-com-leite.svg'
+import CapuccinoImg from '../assets/cups/capuccino.svg'
+import ExpressoCremosoImg from '../assets/cups/expresso-cremoso.svg'
+import ExpressoGeladoImg from '../assets/cups/expresso-gelado.svg'
+import ExpressoTradicionalImg from '../assets/cups/expresso-tradicional.svg'
+import LatteImg from '../assets/cups/latte.svg'
+import ChocolateQuenteImg from '../assets/cups/chocolate-quente.svg'
+import CubanoImg from '../assets/cups/cubano.svg'
+import HavaianoImg from '../assets/cups/havaiano.svg'
+import IrlandesImg from '../assets/cups/irlandes.svg'
+import MacchiatoImg from '../assets/cups/macchiato.svg'
+import MochaccinoImg from '../assets/cups/mochaccino.svg'
+
+export const COFFEE_IMAGES = {
+    Arabe: ArabeImg,
+    ExpressoAmericano: ExpressoAmericanoImg,
+    CafeComLeite: CafeComLeiteImg,
+    Capuccino: CapuccinoImg,
+    ExpressoCremoso: ExpressoCremosoImg,
+    ExpressoGelado: ExpressoGeladoImg,
+    ExpressoTradicional: ExpressoTradicionalImg,
+    Latte: LatteImg,
+    ChocolateQuente: ChocolateQuenteImg,
+    Cubano: CubanoImg,
+    Havaiano: HavaianoImg,
+    Irlandes: IrlandesImg,
+    Macchiato: MacchiatoImg,
+    Mochaccino: MochaccinoImg,
+}
+
