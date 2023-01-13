a6a8d9a93adda507ecf23179032eea2ee9620b9a
*Gustavo <guga77sugu@hotmail.com>
2023-01-13 16:07:18 -0300

    Checkout page begins

--- src/components/pages/Checkout/index.tsx
+++ src/components/pages/Checkout/index.tsx
@@ -1,9 +1,46 @@
+import { MapPinLine } from 'phosphor-react'
 import styles from './checkout.module.css'
 
 export function Checkout() {
 
 
   return (
-    <h1 className={styles.title}>CHECKOUT</h1>
+    <div className={styles.checkoutContainer}>
+      <div>
+        <h3>Complete seu pedido</h3>
+
+        <div className={styles.userDataContainer}>
+
+          <div className={styles.addressinfo}>
+            <MapPinLine size={22} />
+            <div>
+              <span>Endereço de Entrega</span>
+              <p>Informe o endereço onde deseja receber seu pedido</p>
+            </div>
+          </div>
+
+
+          <div className={styles.datasFromUser}>
+            <input style={{ width: "12.5rem" }} placeholder='CEP' type="number" />
+            <input placeholder='Rua' type="text" />
+
+            <div className={styles.datasFromUser2}>
+              <input style={{ width: "12.5rem" }} placeholder='Número' type="number" />
+              <input style={{ width: "21.75rem" }} placeholder='Complemento' type="text" />
+            </div>
+
+            <div className={styles.datasFromUser3}>
+              <input style={{ width: "12.5rem" }} placeholder='Bairro' type="number" />
+              <input style={{ width: "17.25rem" }} placeholder='Cidade' type="text" />
+              <input style={{ width: "3.75rem" }} placeholder='UF' type="text" />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.rightContainer}>
+        <h3>Cafés selecionados</h3>
+      </div>
+    </div>
   )
 }
