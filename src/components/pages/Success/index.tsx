552c62a36708df7557746240398d496b5af1d582
*Gustavo <guga77sugu@hotmail.com>
2023-01-15 16:09:06 -0300

    Success Page complete.

--- src/components/pages/Success/index.tsx
+++ src/components/pages/Success/index.tsx
@@ -1,5 +1,6 @@
 import styles from './success.module.css'
 import illustrationPicture from '../../../assets/illustration.svg'
+import { CurrencyDollar, MapPin, Timer } from 'phosphor-react'
 
 export function Success() {
     return (
@@ -12,9 +13,62 @@ export function Success() {
             <div className={styles.successContainer}>
 
                 <div className={styles.resumePay}>
-                    <h1>asdasfdasdasd</h1>
-                    <h1>asdasfdasdasd</h1>
-                    <h1>asdasfdasdasd</h1>
+                    <div className={styles.envolt}>
+                        <div
+                            style={{
+                                color: 'var(--white)',
+                                width: '2rem',
+                                height: '2rem',
+                                background: `var(--purple)`,
+                                borderRadius: '999px',
+                                padding: '8px',
+                            }}>
+                            <MapPin size={16} />
+                        </div>
+                        <div>
+                            <p style={{ fontSize: '1rem' }}>Entrega em <span>Rua João Daniel Martinelli, 102</span></p>
+                            <p>Farrapos - Porto Alegre, RS</p>
+
+                        </div>
+                    </div>
+
+                    <div className={styles.envolt}>
+                        <div
+                            style={{
+                                color: 'var(--white)',
+                                width: '2rem',
+                                height: '2rem',
+                                background: `var(--yellow)`,
+                                borderRadius: '999px',
+                                padding: '8px',
+                            }}>
+                            <Timer size={16} />
+                        </div>
+                        <div>
+                            <p style={{ fontSize: '1rem' }}>Previsão de entrega</p>
+                            <span>20 min - 30 min</span>
+
+                        </div>
+                    </div>
+
+                    <div className={styles.envolt}>
+                        <div
+                            style={{
+                                color: 'var(--white)',
+                                width: '2rem',
+                                height: '2rem',
+                                background: `var(--yellow-dark)`,
+                                borderRadius: '999px',
+                                padding: '8px',
+                            }}>
+                            <CurrencyDollar size={16} />
+                        </div>
+                        <div>
+                            <p style={{ fontSize: '1rem' }}>Pagamento na entrega</p>
+                            <span>Cartão de crédito</span>
+
+                        </div>
+                    </div>
 
                 </div>
 
