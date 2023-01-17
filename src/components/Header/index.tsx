bf872c3b626d7a425f4eb2a3bb9b2c9d87e29974
*Gustavo <guga77sugu@hotmail.com>
2023-01-17 15:50:27 -0300

    function to disable Shopping Cart Button with NavLink and span from quantity carts in button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,8 +4,14 @@ import { MapPin, ShoppingCart } from 'phosphor-react'
 import { NavLink } from 'react-router-dom'
 
 import styles from './header.module.css'
+import { useContext } from 'react'
+import { CoffeeContext } from '../../contexts/CoffeeContext'
 
 export function Header() {
+  const { coffeeList } = useContext(CoffeeContext)
+  const quantityInCart = coffeeList.length
+  const emptyListaCoffee = quantityInCart <= 0
+
   return (
     <nav
       className={styles.content}>
@@ -20,10 +26,19 @@ export function Header() {
           <p>Porto Alegre, RS</p>
         </div>
 
-        <NavLink to="/checkout" title="Checkout">
-          <div className={styles.shoppingCart}>
+        <NavLink
+          onClick={() => {
+            if (emptyListaCoffee)
+              window.alert('Para ir ao carrinho de compras, você deve primeiro adicionar algum ítem abaixo!')
+          }}
+          to={emptyListaCoffee ? '' : '/checkout'}
+          title="Clique para ir ao carrinho de compras">
+
+          <button disabled={emptyListaCoffee} className={styles.shoppingCart}>
+            {quantityInCart >= 1 ? <span title={`Você tem ${quantityInCart} pedido(os) adicionados ao seu carrinho de compras!`} className={styles.lenghtCart}>{quantityInCart}</span> : ''}
+
             <ShoppingCart size={22} weight="fill" />
-          </div>
+          </button>
         </NavLink>
       </div>
 
